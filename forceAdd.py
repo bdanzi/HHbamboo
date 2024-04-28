279b7e1706aaba5313c69505b73a4455f8bf5353
Brunella D'Anzi <75045014+bdanzi@users.noreply.github.com>
2024-04-28 17:26:15 +0200

    Add files via upload

---
+++ forceAdd.py
@@ -0,0 +1,25 @@
+import os
+import argparse
+import yaml
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--outDir", type = str, help="Output directory of bambooRun", required=True)
+parser.add_argument("--yml", type = str, help="yml sample file", required=True)
+args   = parser.parse_args()
+outDir = args.outDir
+yml    = args.yml
+
+DIR    = f"./{outDir}/batch/output/"
+ADDIR  = f"./{outDir}/results/"
+with open(yml, "r") as stream:
+    try:
+        dictYML =  yaml.safe_load(stream)
+        samples =  dictYML["samples"].keys()
+        print(samples)
+    except yaml.YAMLError as exc:
+        print(exc)
+
+for s in samples:
+    print(s)
+    print(f'find {DIR} -name "{s}.root" | xargs hadd -f {ADDIR}{s}.root') 
+    os.system(f'find {DIR} -name "{s}.root" | xargs hadd -f {ADDIR}{s}.root')
